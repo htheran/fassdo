498bd3e5e87632a6ba8a5cc6178f3f34f95541d4
htheran <htheran@gmail.com>
2024-10-22 13:43:36 -0500

    Se mejora el formulario de ejecutar playbooks en host

--- executor/forms.py
+++ executor/forms.py
@@ -2,11 +2,34 @@ from django import forms
 from inventory.models import Environment, Group, Host
 from playbook.models import Playbook
 
+# service_manager/forms.py
+
+
 class ExecutePlaybookHostForm(forms.Form):
-    environment = forms.ModelChoiceField(queryset=Environment.objects.all(), label='Ambiente')
-    group = forms.ModelChoiceField(queryset=Group.objects.none(), label='Grupo')
-    host = forms.ModelChoiceField(queryset=Host.objects.none(), label='Host')
-    playbook = forms.ModelChoiceField(queryset=Playbook.objects.none(), label='Playbook')
+    environment = forms.ModelChoiceField(
+        queryset=Environment.objects.all(),
+        label='Ambiente',
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-control', 'onchange': 'this.form.submit();'})
+    )
+    group = forms.ModelChoiceField(
+        queryset=Group.objects.none(),
+        label='Grupo',
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-control', 'onchange': 'this.form.submit();'})
+    )
+    host = forms.ModelChoiceField(
+        queryset=Host.objects.none(),
+        label='Host',
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
+    playbook = forms.ModelChoiceField(
+        queryset=Playbook.objects.filter(playbook_type='host'),
+        label='Playbook',
+        required=True,
+        widget=forms.Select(attrs={'class': 'form-control'})
+    )
 
     def __init__(self, *args, **kwargs):
         super(ExecutePlaybookHostForm, self).__init__(*args, **kwargs)
@@ -17,11 +40,6 @@ class ExecutePlaybookHostForm(forms.Form):
                 self.fields['group'].queryset = Group.objects.filter(environment_id=environment_id)
             except (ValueError, TypeError):
                 pass
-        elif self.initial.get('environment'):
-            environment_id = self.initial.get('environment').id
-            self.fields['group'].queryset = Group.objects.filter(environment_id=environment_id)
-        else:
-            self.fields['group'].queryset = Group.objects.none()
 
         if 'group' in self.data:
             try:
@@ -29,44 +47,10 @@ class ExecutePlaybookHostForm(forms.Form):
                 self.fields['host'].queryset = Host.objects.filter(group_id=group_id)
             except (ValueError, TypeError):
                 pass
-        elif self.initial.get('group'):
-            group_id = self.initial.get('group').id
-            self.fields['host'].queryset = Host.objects.filter(group_id=group_id)
-        else:
-            self.fields['host'].queryset = Host.objects.none()
 
-        if 'host' in self.data:
-            try:
-                host_id = int(self.data.get('host'))
-                host = Host.objects.get(id=host_id)
-                operating_system = host.operating_system
-                self.fields['playbook'].queryset = Playbook.objects.filter(
-                    playbook_type='host',
-                    operating_system__iexact=operating_system
-                )
-            except (ValueError, TypeError, Host.DoesNotExist):
-                pass
-        elif self.initial.get('host'):
-            host = self.initial.get('host')
-            operating_system = host.operating_system
-            self.fields['playbook'].queryset = Playbook.objects.filter(
-                playbook_type='host',
-                operating_system__iexact=operating_system
-            )
-        else:
-            self.fields['playbook'].queryset = Playbook.objects.none()
+        # Cargar siempre los playbooks de tipo 'host'
+        self.fields['playbook'].queryset = Playbook.objects.filter(playbook_type='host')
 
-        # Incluir los valores seleccionados en los queryset para evitar errores de validación
-        if self.is_bound:
-            group = self.fields['group'].queryset.filter(pk=self.data.get('group')).first()
-            if group:
-                self.fields['group'].queryset = Group.objects.filter(pk=group.pk)
-            host = self.fields['host'].queryset.filter(pk=self.data.get('host')).first()
-            if host:
-                self.fields['host'].queryset = Host.objects.filter(pk=host.pk)
-            playbook = self.fields['playbook'].queryset.filter(pk=self.data.get('playbook')).first()
-            if playbook:
-                self.fields['playbook'].queryset = Playbook.objects.filter(pk=playbook.pk)
 
 class ExecutePlaybookGroupForm(forms.Form):
     environment = forms.ModelChoiceField(queryset=Environment.objects.all(), label='Ambiente')
